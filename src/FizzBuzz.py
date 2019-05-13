8d435fc0d80e7b6c44aac4bd14cc41d63ef1c0db
KevinGoilard <kevin.goilard@gmail.com>
2019-05-13 11:24:38 +0200

    refactor thirty return FizzBuzz case

--- src/FizzBuzz.py
+++ src/FizzBuzz.py
@@ -2,9 +2,7 @@
 
 class FizzBuzz:
     def compute_string(self, param):
-        if param == 30:
-            return "FizzBuzz"
-        if param == 15:
+        if param % 3 == 0 and param % 5 == 0:
             return "FizzBuzz"
 
         if param % 5 == 0:
