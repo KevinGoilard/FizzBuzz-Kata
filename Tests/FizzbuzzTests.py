5b77d3dec69505fa3089e014575f7606123ddc5e
KevinGoilard <kevin.goilard@gmail.com>
2019-05-13 11:29:07 +0200

    refactor compute_string is now get_representation_for

--- Tests/FizzbuzzTests.py
+++ Tests/FizzbuzzTests.py
@@ -8,49 +8,49 @@ class FizzBuzzTests(unittest.TestCase):
         self.assertTrue(True)
 
     def test_one_return_one(self):
-        result = FizzBuzz.compute_string(1)
+        result = FizzBuzz.get_representation_for(1)
 
         expected = 1
         self.assertEqual(expected, result)
 
     def test_two_return_two(self):
-        result = FizzBuzz.compute_string(2)
+        result = FizzBuzz.get_representation_for(2)
 
         expected = 2
         self.assertEqual(expected, result)
 
     def test_three_return_Fizz(self):
-        result = FizzBuzz.compute_string(3)
+        result = FizzBuzz.get_representation_for(3)
 
         expected = "Fizz"
         self.assertEqual(expected, result)
 
     def test_six_return_Fizz(self):
-        result = FizzBuzz.compute_string(6)
+        result = FizzBuzz.get_representation_for(6)
 
         expected = "Fizz"
         self.assertEqual(expected, result)
 
     def test_five_return_Buzz(self):
-        result = FizzBuzz.compute_string(5)
+        result = FizzBuzz.get_representation_for(5)
 
         expected = "Buzz"
         self.assertEqual(expected, result)
 
     def test_ten_return_Buzz(self):
-        result = FizzBuzz.compute_string(10)
+        result = FizzBuzz.get_representation_for(10)
 
         expected = "Buzz"
         self.assertEqual(expected, result)
 
     def test_fifteen_return_FizzBuzz(self):
-        result = FizzBuzz.compute_string(15)
+        result = FizzBuzz.get_representation_for(15)
 
         expected = "FizzBuzz"
         self.assertEqual(expected, result)
 
     def test_thirty_return_FizzBuzz(self):
-        result = FizzBuzz.compute_string(30)
+        result = FizzBuzz.get_representation_for(30)
 
         expected = "FizzBuzz"
         self.assertEqual(expected, result)
--- src/FizzBuzz.py
+++ src/FizzBuzz.py
@@ -2,14 +2,14 @@
 
 class FizzBuzz:
     @staticmethod
-    def compute_string(param):
-        if param % 3 == 0 and param % 5 == 0:
+    def get_representation_for(number):
+        if number % 3 == 0 and number % 5 == 0:
             return "FizzBuzz"
 
-        if param % 5 == 0:
+        if number % 5 == 0:
             return "Buzz"
 
-        if param % 3 == 0:
+        if number % 3 == 0:
             return "Fizz"
 
-        return param
+        return number
