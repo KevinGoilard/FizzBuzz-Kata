d172a1783ceede90fb7e6242f48d0366c8a8d8b9
KevinGoilard <kevin.goilard@gmail.com>
2019-05-13 11:11:40 +0200

    refactor six returns Fizz case

--- src/FizzBuzz.py
+++ src/FizzBuzz.py
@@ -2,8 +2,6 @@
 
 class FizzBuzz:
     def compute_string(self, param):
-        if param == 6:
-            return "Fizz"
-        if param == 3:
+        if param % 3 == 0:
             return "Fizz"
         return 1
