2d71d0e8183cb920a6b6a60d27fa58ffb2d386ca
KevinGoilard <kevin.goilard@gmail.com>
2019-05-13 11:17:48 +0200

    Refactor two return two case

--- src/FizzBuzz.py
+++ src/FizzBuzz.py
@@ -2,12 +2,10 @@
 
 class FizzBuzz:
     def compute_string(self, param):
-        if param == 2:
-            return 2
-
         if param == 5:
             return "Buzz"
 
         if param % 3 == 0:
             return "Fizz"
-        return 1
+
+        return param
